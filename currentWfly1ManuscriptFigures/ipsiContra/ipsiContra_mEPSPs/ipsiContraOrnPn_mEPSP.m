013cbc76651359e3f99be5462d71d93edb776cb7
wclee <darbly@gmail.com>
2016-03-17 12:43:08 -0400

    adding mean ipsiContra mEPSP panel

---
+++ currentWfly1ManuscriptFigures/ipsiContra/ipsiContra_mEPSPs/ipsiContraOrnPn_mEPSP.m
@@ -0,0 +1,148 @@
+% This code relies on the products of mEPSP_AmpWorking
+
+% Collect the amplitude of ipsi and contra MEPSPs for R and L ORN-->PN
+% pairs
+        
+    %loop over PNs
+    for p=1:5
+        
+        if p<=3
+            
+            counter=1;
+            for s=1:size(leftMEPSPs{p},1)
+                
+            indAmpsI{p}(counter)=max(leftMEPSPs{p}(s,:))-mean(leftMEPSPs{p}(s,1:40));
+            counter=counter+1;
+            
+            end
+            
+            counter=1;
+            for s=1:size(rightMEPSPs{p},1)
+                
+                indAmpsC{p}(counter)=max(rightMEPSPs{p}(s,:))-mean(rightMEPSPs{p}(s,1:40));
+                counter=counter+1;
+                
+            end
+            
+            
+            
+        else
+                
+                
+            counter=1;
+            for s=1:size(leftMEPSPs{p},1)
+                
+            indAmpsC{p}(counter)=max(leftMEPSPs{p}(s,:))-mean(leftMEPSPs{p}(s,1:40));
+            counter=counter+1;
+            
+            end
+            
+            counter=1;
+            for s=1:size(rightMEPSPs{p},1)
+                
+                indAmpsI{p}(counter)=max(rightMEPSPs{p}(s,:))-mean(rightMEPSPs{p}(s,1:40));
+                counter=counter+1;
+                
+            end
+            
+            
+    
+        end
+        
+        miniMeans(p,1)=mean(indAmpsI{p});
+        miniMeans(p,2)=mean(indAmpsC{p});
+        
+        sem(p,1)=std(indAmpsI{p})/sqrt(length(indAmpsI{p}))
+        sem(p,2)=std(indAmpsC{p})/sqrt(length(indAmpsC{p}))
+    end
+  
+% 
+% figure()
+% 
+% scatter(mean(ipL_uEPSP'),conL_uEPSP)
+% hold on
+% scatter(ipR_uEPSP, mean(conR_uEPSP'), 'r')
+% set(gcf, 'Color', 'w')
+% ax=gca;
+% % ax.XTick=[0:10:110];
+% % ax.YTick=[0:10:110];
+% % xlim([0 110]);
+% % ylim([0 110]);
+% ax.FontSize=16;
+% refline(1,0)
+% xlabel('ipsi ORN--PN uEPSP Amp (mV)', 'FontSize', 16);
+% ylabel('contra ORN--PN uEPSP Amp (mV)', 'FontSize', 16);
+
+%%Plotting
+
+
+%% collecting ipsi-contra mEPSPs assuming
+
+ipsi_mEPSPs = cell2mat(indAmpsI)';
+contra_mEPSPs = cell2mat(indAmpsC)';
+
+% ipsi_mEPSPs = miniMeans(:,1);
+% contra_mEPSPs =  miniMeans(:,2);
+
+%% simple bars
+gps = [ones(size(ipsi_mEPSPs));2.*ones(size(contra_mEPSPs))];
+vals = [ipsi_mEPSPs;contra_mEPSPs];
+[Ymean,Ysem,Ystd,Yci] = grpstats(vals,gps,{'mean','sem','std','meanci'});
+
+figure
+bar([mean(ipsi_mEPSPs),mean(contra_mEPSPs)],.4,'FaceColor','k','LineWidth',2)
+hold on
+he = errorbar(Ymean,Ysem,'k','LineStyle','none'); % error bars are sem
+he.LineWidth=1;
+xlim([0.5 2.5])
+% ylim([0 30])
+ax = gca;
+ax.XTick = [1 2];
+ax.XTickLabel = {'Ipsi';'Contra'};
+ylabel('mEPSP Amplitude (mV)')
+axis square
+
+%% Permutation test (p = ~0.001)
+nPerm = 100000;
+
+sa = ipsi_mEPSPs;
+sb = contra_mEPSPs;
+
+sh0 = [sa; sb];
+
+m = length(sa); 
+n = length(sb); 
+
+d_empirical = mean(sa) - mean(sb);
+
+sa_rand = zeros(m,nPerm);
+sb_rand = zeros(n,nPerm);
+tic
+for ii = 1:nPerm
+    sa_rand(:,ii) = randsample(sh0,m);%,true);
+    sb_rand(:,ii) = randsample(sh0,n);%,true);
+end
+toc
+% Now we compute the differences between the means of these resampled
+% samples.
+% d = median(sb_rand) - median(sa_rand);
+d = mean(sa_rand) - mean(sb_rand);
+
+%
+figure;
+% [nn,xx] = hist(d,100);
+% bar(xx,nn/sum(nn))
+histogram(d,'Normalization','probability')
+ylabel('Probability of occurrence')
+xlabel('Difference between means')
+hold on
+%
+
+y = get(gca,'yLim'); % y(2) is the maximum value on the y-axis.
+x = get(gca,'xLim'); % x(1) is the minimum value on the x-axis.
+plot([d_empirical,d_empirical],y*.99,'r-','lineWidth',2)
+
+% Probability of H0 being true = 
+% (# randomly obtained values > observed value)/total number of simulations
+p = sum(abs(d) > abs(d_empirical))/length(d);
+text(x(1)+(.01*(abs(x(1))+abs(x(2)))),y(2)*.95,sprintf('H0 is true with %4.4f probability.',p))
