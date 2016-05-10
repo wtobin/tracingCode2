f3d3a5d7c886e457c0f166e278119002f99ad080
Willie Tobin <wtobin1111@gmail.com>
2016-05-10 18:09:50 -0400

    more supplamental work

--- currentWfly1ManuscriptFigures/Supplamentals/ipsiContraLengthsDens/ipsiContraLenghtsDens.m
+++ currentWfly1ManuscriptFigures/Supplamentals/ipsiContraLengthsDens/ipsiContraLenghtsDens.m
@@ -13,15 +13,16 @@ ORNs_Right=annotations.Right_0x20_ORN;
 ORNs=[ORNs_Left, ORNs_Right];
 
 %return all skeleton IDs of DM6 PNs
-PNs=annotations.DM6_0x20_PN;
+PNs=sort(annotations.DM6_0x20_PN);
 
 %% Calculate Path length
 
-load('~/Documents/MATLAB/tracingCode2/currentWfly1ManuscriptFigures/ornOutputSynDensity/leftAxons')
-load('~/Documents/MATLAB/tracingCode2/currentWfly1ManuscriptFigures/ornOutputSynDensity/rightAxons')
+load('~/Documents/MATLAB/tracingCode2/currentWfly1ManuscriptFigures/Supplamentals/ornOutputSynDensity/leftAxons')
+load('~/Documents/MATLAB/tracingCode2/currentWfly1ManuscriptFigures/Supplamentals/ornOutputSynDensity/rightAxons')
 
-
-% For each ORN calculate its total ipsi within glomerulus length
+% For each ORN calculate its total within glomerulus axon lengths
+leftCounter=1;
+rightCounter=1;
 
 for p=1:length(ORNs)
     
@@ -36,51 +37,68 @@ for p=1:length(ORNs)
         
     end
     
-    leftLengths(p)=sum(leftRunL);
-    leftPreSites(p)=sum(leftRunPS);
-    
+    if isnan(leftRunPS)==1
+        
+    else
+        
+        leftLengths(leftCounter)=sum(leftRunL);
+        leftPreSites(leftCounter)=sum(leftRunPS);
+        leftCounter=leftCounter+1;
+        
+    end
     
     rightRunL=[];
     rightRunPS=[];
     
     
     for j=1:size(rightAxons{p},2)
+        
         rightRunL=[rightRunL, rightAxons{p}(1,j)];
         rightRunPS=[rightRunPS,rightAxons{p}(2,j)];
+        
     end
     
-    rightLengths(p)=sum(rightRunL);
-    rightPreSites(p)=sum(rightRunPS);
-    
+    if isnan(rightRunPS)==1
+    else
+        
+        rightLengths(rightCounter)=sum(rightRunL);
+        rightPreSites(rightCounter)=sum(rightRunPS);
+        rightCounter=rightCounter+1;
+    end
 end
+
 
 
 %% create array of length and Density for ipsi and Contra axons
 
 
-ipsiLenDens(:,1)=[leftLengths(1:27),rightLengths(28:end)];
-ipsiLenDens(:,2)=[leftPreSites(1:27)./leftLengths(1:27),rightPreSites(28:end)./rightLengths(28:end)];
+ipsiLenDens(:,1)=[leftLengths(1:27),rightLengths(27:end)];
+ipsiLenDens(:,2)=[leftPreSites(1:27)./leftLengths(1:27),rightPreSites(27:end)./rightLengths(27:end)];
 
-contraLenDens(:,1)=[leftLengths(28:end),rightLengths(1:27)];
-contraLenDens(:,2)=[leftPreSites(28:end)./leftLengths(28:end),rightPreSites(1:27)./rightLengths(1:27)];
+contraLenDens(:,1)=[leftLengths(28:end),rightLengths(1:26)];
+contraLenDens(:,2)=[leftPreSites(28:end)./leftLengths(28:end),rightPreSites(1:26)./rightLengths(1:26)];
 
 %% Plotting
 figure()
 
 set(gcf,'Color','w')
-subplot(1,2,1)
-boxplot([ipsiLenDens(:,1),contraLenDens(:,1)])
+boxplot([ipsiLenDens(:,1);contraLenDens(:,1)], [ones(53,1);2*ones(51,1)], 'color', 'k')
 ax=gca;
 ax.XTickLabel={'ipsi axons','contra axons'};
-ylabel('cable length(nm)');
+ax.FontSize=16;
+ylabel('cable length(nm)','FontSize',16);
+axis square
+saveas(gcf,'ipsiContraLength')
+saveas(gcf,'ipsiContraLength','png')
 
 
-
-subplot(1,2,2)
-boxplot([ipsiLenDens(:,2),contraLenDens(:,2)])
+figure()
+set(gcf,'Color','w')
+boxplot([ipsiLenDens(:,2);contraLenDens(:,2)], [ones(53,1);2*ones(51,1)],'color', 'k')
 ax=gca;
 ax.XTickLabel={'ipsi axons','contra axons'};
-ylabel('tbar density (tbars/nm)');
-
-saveas(gcf,'ipsiContraLenDens')
-
+ylabel('tbar density (tbars/nm)', 'FontSize',16);
+axis square
+ax.FontSize=16;
+saveas(gcf,'ipsiContraDens')
+saveas(gcf,'ipsiContraDens','png')
