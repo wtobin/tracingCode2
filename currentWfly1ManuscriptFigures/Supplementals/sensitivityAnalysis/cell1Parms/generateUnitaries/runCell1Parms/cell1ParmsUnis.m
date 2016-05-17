54e0506e50fdd9b9cdd15bfb9581ffb239111a90
Willie Tobin <wtobin1111@gmail.com>
2016-05-17 11:45:59 -0400

    work completed on sensitivity analysis

---
+++ currentWfly1ManuscriptFigures/Supplementals/sensitivityAnalysis/cell1Parms/generateUnitaries/runCell1Parms/cell1ParmsUnis.m
@@ -0,0 +1,96 @@
+%The goal of this piece of code is to change PN model parameters to Gouwens & Wilson cell #1 and rerun
+%the mini and unitary analyses
+
+%% setup the simulation folder and modify the  NEURON text files
+
+for p = 2:5
+   
+PN_Names={'PN1LS','PN2LS', 'PN3LS', 'PN1RS', 'PN2RS'};
+PN=cell2mat(PN_Names(p)); 
+
+%Move to the PN  project directory
+cd(['~/nC_projects/',PN,'_allORNs/'])
+
+%make a dir in simulations called detTask
+system('mkdir simulations/cell1Parms')
+
+ %path to the dir containing the hoc files to be run
+path1=['~/nC_projects/',PN,'_allORNs/simulations/cell1Parms/'];
+cd(path1)
+ 
+
+% Copy the contents of the generatedNEURON dir to detTask
+system('cp -a ../../generatedNEURON/. ./')
+
+% Copy the synaptic conductance model that has been tuned to these parms
+% into this dir
+system('cp -a ../../../PN1LS_allORNs/simulations/cell1ParmsCondAdj/DoubExpSynA.mod ./')
+
+%get the neuron hoc file name
+neuronFileName=dir('neuron*.hoc');
+neuronFileName=neuronFileName.name;
+
+%replace the value for axial resistance in the neuron hoc file 
+Ra=163.9 ; %in ohm*cm
+chngRaCmd=['sed -i -e ''s#Ra = 266.1#Ra = ',num2str(Ra),'#'' ',neuronFileName];
+system(chngRaCmd)
+
+
+%replace the value for specific membrane capacitance in the neuron hoc file 
+cm=2.6 ; %in uF*cm^-2
+chngCmCmd=['sed -i -e ''s#cm = 0.8#cm = ',num2str(cm),'#'' ',neuronFileName];
+system(chngCmCmd)
+
+%replace the value for gmax of leak conductance in the neuron hoc file 
+gmax=1/(8.3*1000) ; %in uF*cm^-2
+%multiplication of gmax here is simply to match the format in the neuron
+%file, I hardcode the E-4
+chngGmaxCmd_nrnFile=['sed -i -e ''s#gmax_LeakConductance = 4.8E-5#gmax_LeakConductance = ',num2str(gmax*10000,2),'E-4#'' ',neuronFileName];
+system(chngGmaxCmd_nrnFile)
+
+%replace the value for gmax in the LeakConductance.mod file 
+gmax=1/(8.3*1000) ; %in uF*cm^-2
+chngGmaxCmd=['sed -i -e ''s#gmax = 0.000048#gmax = ',num2str(gmax,'%10f'),'#'' LeakConductance.mod'];
+system(chngGmaxCmd)
+
+%replace the value for gmax in the LeakConductance.mod file, this value is
+%listed at the top of the file in different units and should be written
+%over but I am going to change it anyway
+gmax=1/(8.3) ; %in ?
+chngGmaxCmd=['sed -i -e ''s#gmax = 0.048#gmax = ',num2str(gmax,'%10f'),'#'' LeakConductance.mod'];
+system(chngGmaxCmd)
+
+%Compile mod files in this Dir
+system('nrnivmodl')
+
+%run hocEd3 on the sim hoc file, ?sets up unitary simulation
+hocEdCmd=['python ../../../hocEd3.py ',PN,'_allORNs.hoc '];
+system(hocEdCmd)
+
+%replace the simsDirPath
+system(['sed -i -e ''s#\/simulations\/#\/simulations\/cell1Parms\/#'' ', PN,'_allORNs.hoc'])
+
+
+% make a directory to store the results
+system('mkdir unitaries')
+
+%replace the simReference file
+system(['sed -i -e ''s#"Sim_1"#"unitaries"#'' ', PN,'_allORNs.hoc'])
+
+
+%Set initial Vm
+initVm=-60; %in mv
+runVCmd=['sed -i -e ''s#v\s\=\s\-65\.\0#v = \',num2str(initVm),'#'' ', PN,'_allORNs.hoc'];
+system(runVCmd)
+
+
+%Setsim duration
+runTime=11000; %in ms
+runTCmd=['sed -i -e ''s#tstop\s\=\s.*#tstop \= ',num2str(runTime),'#'' ',PN,'_allORNs.hoc'];
+system(runTCmd)
+
+% Run the simulation
+system(['nrniv ',PN,'_allORNs.hoc'])
+
+
+end
---
+++ currentWfly1ManuscriptFigures/Supplementals/sensitivityAnalysis/cell2Parms/generateUnitaries/tuneCell2ParmsSynCond/cell2ParmsSynConTuning.m
@@ -0,0 +1,78 @@
+%The goal of this piece of code is to adjust the modeled synaptic
+%conductance to match EPSP amplitude between models with different
+%parameters
+
+%% setup the simulation folder and modify the  NEURON text files
+
+   
+PN='PN1LS';
+
+%Move to the PN1 LS project directory
+cd(['~/nC_projects/',PN,'_allORNs/'])
+
+%make a dir in simulations called detTask
+system('mkdir simulations/cell2ParmsCondAdj')
+
+ %path to the dir containing the hoc files to be run
+path1=['~/nC_projects/',PN,'_allORNs/simulations/cell2ParmsCondAdj/'];
+cd(path1)
+ 
+% Copy the contents of the cell2Parms dir to this dir
+system('cp -a ../cell2Parms/. ./')
+
+%run the first 300ms of the simulation
+
+%Setsim duration
+runTime=300; %in ms
+runTCmd=['sed -i -e ''s#tstop\s\=\s.*#tstop \= ',num2str(runTime),'#'' ',PN,'_allORNs.hoc'];
+system(runTCmd)
+
+%replace the simReference file
+system(['sed -i -e ''s#"unitaries"#"output"#'' ', PN,'_allORNs.hoc'])
+
+%replace the simsDirPath
+system(['sed -i -e ''s#\/simulations\/cell2Parms\/#\/simulations\/cell2ParmsCondAdj\/#'' ', PN,'_allORNs.hoc'])
+
+%make output directory
+
+system('mkdir output')
+
+% Run the simulation
+system(['nrniv ',PN,'_allORNs.hoc'])
+
+%Import the resulting uEPSP
+testP=load('./output/neuron_PN1_LS_sk_419138_0.dat');
+
+%Calculate the percent difference between this max amp and that w/ cell3
+%parms
+discrep=((max(testP)+60)/(5.0819))-1;
+
+while abs(discrep)>0.005
+
+% Pull gmax value from the DoubExpSynA.mod file
+[s, gmax]=system('grep -oP ''gmax = \K.*'' DoubExpSynA.mod');
+
+
+%replace this value w/ ascaled version based on the diff value calculated
+%above
+
+new_gmax=str2num(gmax)+((-discrep)*str2num(gmax))
+chngGCmd=['sed -i -e ''s#gmax = ',gmax(1:end-1),'#gmax = ',num2str(new_gmax),'#'' DoubExpSynA.mod'];
+system(chngGCmd)
+
+%Compile mod files in this Dir
+system('nrnivmodl')
+
+% Run the simulation
+system(['nrniv ',PN,'_allORNs.hoc'])
+
+%Import the resulting uEPSP
+testP=load('./output/neuron_PN1_LS_sk_419138_0.dat');
+
+%Calculate the percent difference between this max amp and that w/ cell3
+%parms
+discrep=((max(testP)+60)/(5.0819))-1;
+
+end
+
+system('cp DoubExpSynA.mod ../cell2Parms/')
