d1f34fb258db2d786ad2cbe34f45d79cb1c1563f
William Tobin <williamtobin@Williams-MacBook-Pro.local>
2017-03-14 19:03:13 -0400

    added code for and generated synapse size/number varation histograms

---
+++ currentWfly1ManuscriptFigures/Supplementals/meanSizeSynNum/ipsiMeanPNAreaDist/ipsiMeanPNAreaDist.m
@@ -0,0 +1,58 @@
+%Load the cell array contianing the tracer averaged, bias corrected tbar vol and pn area measurments
+load '/Users/williamtobin/Documents/MATLAB/tracingCode2/synapseSizeScripts/aveSizesBC.mat'
+
+%% Determine the mean size of each ORNs ipsi and Contra postsynaptic PN JUST IPSI FOR FINAL TO MATCH SYN NUM HIST
+% membrane area
+
+ipsiPnMeans=[];
+%contraPNMeans=[];
+
+for o=1:10
+    
+    ipsiConns=[];
+    
+    if o<=5
+        ipsiConns=aveSizesBC(o,[1,2,5]);
+        %contraConns=aveSizesBC(o,[3,4]);
+        
+    else
+        
+        ipsiConns=aveSizesBC(o,[3,4]);
+        %contraConns=aveSizesBC(o,[1,2,5]);
+        
+    end
+    
+    for i=1:numel(ipsiConns)
+        
+        ipsiPnMeans=[ipsiPnMeans;mean(ipsiConns{i}(:,2))];
+        
+    end
+    
+%     for j=1:numel(contraConns)
+%         
+%         contraPNMeans=[contraPNMeans;mean(contraConns{j}(:,2))];
+%         
+%     end
+%     
+    
+    
+end
+
+
+%% Plotting
+
+figure()
+set(gcf, 'Color', 'w')
+h1=histogram(ipsiPnMeans, 'FaceColor','k');
+% h1=histogram(pooledNormIpsiOrnToP, 'FaceColor','k');
+xlim([0 2*mean(ipsiPnMeans)])
+ax = gca;
+ax.FontSize=16;
+ylabel('Frequency')
+xlabel('Mean Postsynaptic PN Membrane Area (nm^2)')
+% axis square
+text(2.25*10^4, 15, ['CV: ',num2str(std(ipsiPnMeans)/mean(ipsiPnMeans))], 'FontSize',16)
+box off 
+saveas(gcf,'ipsiMeanPNAreaDist','epsc')
+saveas(gcf,'ipsiMeanPNAreaDist')
+    
